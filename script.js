8cd2db2c7b42b94c970999506112843bdd4423a5
mohamed eldaly <21154454+mohamedeldaly@users.noreply.github.com>
2026-04-11 23:27:33 +0200

    add function to get human choice

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function getComputerChoice() {
-    let random = Math.floor(Math.random() * 3);
-    if (random === 0) {
-        return "rock";
-    } else if (random === 1) {
-        return "paper"
-    } else {
-        return "scissors"
-    }
+  let random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    return "rock";
+  } else if (random === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
-console.log(getComputerChoice())
+function getHumanChoice() {
+  let choice = prompt("choose rock, paper or scissors");
+  return choice;
+}
