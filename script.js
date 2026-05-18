faf7a2e8796ddf97ac6a536cb342cd9bd6d89dff
mohamed eldaly <21154454+mohamedeldaly@users.noreply.github.com>
2026-05-19 01:10:58 +0300

    add ui

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const buttons = document.querySelectorAll("button");
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 3);
   if (random === 0) {
@@ -8,29 +9,41 @@ function getComputerChoice() {
     return "scissors";
   }
 }
-function getHumanChoice() {
-  let choice = prompt("choose rock, paper or scissors");
-  return choice.toLowerCase();
+const roundWinner = document.querySelector("#current");
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    roundWinner.textContent = "you win";
+    humanScore++;
+  } else if (humanChoice === computerChoice) {
+    roundWinner.textContent = "draw";
+  } else {
+    roundWinner.textContent = `you lose ${computerChoice} beat ${humanChoice}`;
+    computerScore++;
+  }
 }
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-  function playRound(humanChoice, computerChoice) {
-    if (
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "paper" && computerChoice === "rock") ||
-      (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-      console.log("you win");
-      humanScore++;
-    } else if (humanChoice === computerChoice) {
-      console.log("draw");
-    } else {
-      console.log(`you lose ${computerChoice} beat ${humanChoice}`);
-      computerScore++;
+const humanChoice = document.querySelector("#human");
+const computerChoice = document.querySelector("#computer");
+const winner = document.querySelector("#winner");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playRound(e.target.textContent, getComputerChoice());
+    humanChoice.textContent = `human score: ${humanScore}`;
+    computerChoice.textContent = `computer score: ${computerScore}`;
+    if (humanScore === 5) {
+      winner.textContent = "human won";
+    } else if (computerScore === 5) {
+      winner.textContent = "computer won";
     }
-  }
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-}
+    if (winner.textContent !== "") {
+      buttons.forEach((button) => {
+        button.disabled = true;
+      });
+    }
+  });
+});
